9f42135b62ad638d12ba37cd09ddcc173d7f3d68
Muyoba Lutlas <lutakachihota@gmail.com>
2020-07-28 11:02:04 +0200

    theLessonContinues

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,67 +1,46 @@
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
-import kotlin.math.pow
-import kotlin.math.roundToInt
+
 
 fun main(args: Array<String>) {
 
-    exampleOf("just") {
-        val observable = Observable.just(listOf(1))
-        observable.subscribe { println(it)}
-    }
-
-    exampleOf("fromIterable") {
-        val observable: Observable<Int> =
-                Observable.fromIterable(listOf(1, 2, 3))
-        observable.subscribe { println(it)}
-    }
-
-    exampleOf("subscribe") {
-        val observable = Observable.just(1, 2, 3)
-        observable.subscribe { println(it) }
-    }
-
-    exampleOf("empty") {
-        val observable = Observable.empty<Unit>()
-        observable.subscribeBy(
-                // 1
-                onNext = { println(it) },
-                // 2
-                onComplete = { println("Completed") }
-        )
-    }
-
-    exampleOf("never") {
-        val observable = Observable.never<Any>()
-
-        observable.subscribeBy(
-                onNext = { println(it) },
-                onComplete = { println("Completed") }
-        )
-    }
-
-    exampleOf("range") {
+    exampleOf("CompositeDisposable") {
         // 1
-        val observable: Observable<Int> = Observable.range(1, 10)
-
-        observable.subscribe {
-            // 2
-            val n = it.toDouble()
-            val fibonacci = ((1.61803.pow(n) - 0.61803.pow(n)) /
-                    2.23606).roundToInt()
-            println(fibonacci)
-        }
-    }
-
-    exampleOf("dispose") {
-        // 1
-        val mostPopular: Observable<String> = Observable.just("A", "B", "C")
+        val subscriptions = CompositeDisposable()
         // 2
-        val subscription = mostPopular.subscribe {
-            // 3
-            println(it)
-        }
+        val disposable = Observable.just<String>("A", "B", "C")
+                .subscribe {
+                    // 3
+                    //println(it)
+                }
+        // 4
+        subscriptions.add(disposable)
     }
+
+    exampleOf("create") {
+
+        val disposables = CompositeDisposable()
+
+        val observableDisposable = Observable.create<String> { emitter ->
+            // 1
+            emitter.onNext("1")
+
+            // 2
+            //emitter.onError(RuntimeException("Error"))
+            //emitter.onComplete()
+
+            // 3
+            emitter.onNext("?")
+        }.subscribeBy(
+                onNext = { println(it) },
+                onComplete = { println("Completed") },
+                onError = { println(it) }
+        )
+
+        disposables.add(observableDisposable)
+    }
+
 
 }
 
